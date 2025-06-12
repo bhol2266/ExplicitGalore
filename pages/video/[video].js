c1dd7e1e4c54ea65179f56cd4dc9ffee1eaa9f82
bhol2266 <73753812+bhol2266@users.noreply.github.com>
2025-06-12 21:01:27 +0545

    Update [video].js

--- pages/video/[video].js
+++ pages/video/[video].js
@@ -162,15 +162,15 @@ const Videoplayer = () => {
             {preloadedVideoQuality.length > 0 && !noVideo ? (
                 <>
                     <Head>
-                      <title>{`${videoDetails.Title} | ExplicitGalore`}</title>
+                     <title>{`${noVideo ? videoTitleBackup : videoDetails.Title} - ${noVideo ? "720p" : videolinkQualitiesScreenshots.video_qualities_available[videolinkQualitiesScreenshots.video_qualities_available.length - 1].toUpperCase()}`} | ExplicitGalore</title>
                         <meta name="description" content={`${capitalizeFirstLetter(noVideo ? videoTitleBackup : videoDetails.Title)} sex video`} />
                         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
                         <meta name="keywords" content={tagString} />
-                        <meta property="og:title" content={`${videoDetails.Title} | ExplicitGalore`} />
+                        <meta property="og:title" content={`${noVideo ? videoTitleBackup : videoDetails.Title} - ${noVideo ? "720p" : videolinkQualitiesScreenshots.video_qualities_available[videolinkQualitiesScreenshots.video_qualities_available.length - 1].toUpperCase()}`} />
                         <meta property="og:description" content={`${capitalizeFirstLetter(noVideo ? videoTitleBackup : videoDetails.Title)} sex video`} />
-                        <meta name="twitter:title" content={`${videoDetails.Title} | ExplicitGalore`} />
+                        <meta name="twitter:title" content={`${noVideo ? videoTitleBackup : videoDetails.Title} - ${noVideo ? "720p" : videolinkQualitiesScreenshots.video_qualities_available[videolinkQualitiesScreenshots.video_qualities_available.length - 1].toUpperCase()}`} />
                         <meta name="twitter:description" content={`${capitalizeFirstLetter(noVideo ? videoTitleBackup : videoDetails.Title)} sex video`} />
-                         <link rel="canonical" href={`https://www.ExplicitGalore.com/video/${video}`} />
+                        <link rel="canonical" href={`https://www.ExplicitGalore.com/video/${video}`} />
 
                     </Head>
 
